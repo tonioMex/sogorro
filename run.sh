c79755e82892a5a718b5c9fa80e631c7402abbf4
toniomex <toniomex316@github.com>
2024-10-30 21:13:21 +0800

    Write run shell script

---
+++ run.sh
@@ -0,0 +1,7 @@
+export GOOGLE_CLOUD_PROJECT="sogorro-62c3f"
+export GOOGLE_APPLICATION_CREDENTIALS="/home/u52561/sogorro-62c3f-firebase-adminsdk-bltx8-6fc57f60dd.json"
+export PORT=8080
+export LINE_API_ENDPOINT="https://api.line.me/v2/bot/message/push"
+export LINEBOT_ACCESS_TOKEN=
+
+go run main.go
